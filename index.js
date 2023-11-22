75553179237dc279e2e8d27625abce6b0b2c7927
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-22 15:17:37 +0700

    ✅ add react native paper

--- index.js
+++ index.js
@@ -6,4 +6,24 @@ import {AppRegistry} from 'react-native';
 import Home from './src/screen/Home';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Home);
+import * as React from 'react';
+import {MD3LightTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <Home />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
