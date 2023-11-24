6537cf5dc690920db9ff00547cdea33d62a8400c
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-24 11:58:10 +0700

    🔷 update styling to heatcard

--- src/components/cards/HeatCard.jsx
+++ src/components/cards/HeatCard.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -8,36 +9,41 @@ import {
   useColorScheme,
   View,
   ImageBackground,
-  Image
+  Image,
 } from 'react-native';
 
-import { useTheme } from 'react-native-paper';
+import {useTheme} from 'react-native-paper';
 
-export default function HeatCard({ image, title, category, rating }) {
-  const theme = useTheme()
+export default function HeatCard({image, title, category, rating, style}) {
+  const theme = useTheme();
 
   const styles = StyleSheet.create({
-    cardBody : { 
-      width: '100%', 
-      display: 'flex', 
-      flexDirection: 'row', 
-      gap: 15, 
-      padding: 10, 
-      borderRadius: 12 , 
+    cardBody: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+      gap: 15,
+      padding: 10,
+      borderRadius: 12,
       backgroundColor: '#fff',
       marginVertical: 5,
-    }
-  })
+    },
+  });
 
   return (
-    <View style={styles.cardBody} >
+    <View style={{...styles.cardBody, ...style}}>
       <Image style={{height: 60, width: 60, borderRadius: 12}} source={image} />
 
       <View>
-        <Text style={{fontSize: 16, fontWeight: 500, color:theme.colors.tmBlack}}>{title}</Text>
-        <Text style={{fontSize: 12, color:theme.colors.gray40}}>{category}</Text> 
-        <Text style={{fontSize: 12, color:theme.colors.gray40}}>{rating}</Text>
+        <Text
+          style={{fontSize: 16, fontWeight: 500, color: theme.colors.tmBlack}}>
+          {title}
+        </Text>
+        <Text style={{fontSize: 12, color: theme.colors.gray40}}>
+          {category}
+        </Text>
+        <Text style={{fontSize: 12, color: theme.colors.gray40}}>{rating}</Text>
       </View>
     </View>
-  )
+  );
 }
