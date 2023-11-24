42fdf6f1481ec807a9df15f99dda4030da9b1f10
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-11-24 12:31:59 +0700

    🔷 update Badge Category Styling

--- src/components/BagdeCategory.jsx
+++ src/components/BagdeCategory.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
 import {
-	SafeAreaView,
-	ScrollView,
-	StatusBar,
-	StyleSheet,
-	Text,
-	useColorScheme,
-	View,
-	ImageBackground,
-	Image
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  ImageBackground,
+  Image,
 } from 'react-native';
+import {useTheme} from 'react-native-paper';
 
 export default function BagdeCategory({title, imageSource}) {
+  const theme = useTheme();
+
   return (
-	<View style={{display: 'flex', flexDirection: 'column'}}>
-		<View style={{height: 60, width: 60}}>
-			<Image style={{height: '100%', width: '100%', borderRadius: 8}} source={imageSource} />
-		</View>
-		<Text style={{textAlign: 'center'}}>{title}</Text>
-	</View>
-  )
+    <View style={{display: 'flex', flexDirection: 'column'}}>
+      <View style={{height: 60, width: 60}}>
+        <Image
+          style={{height: '100%', width: '100%', borderRadius: 8}}
+          source={imageSource}
+        />
+      </View>
+      <Text style={{textAlign: 'center', color: theme.colors.tmBlack}}>
+        {title}
+      </Text>
+    </View>
+  );
 }
