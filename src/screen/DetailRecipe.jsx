2225bb971198b709961584226a2876af81634298
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-12-01 22:37:25 +0700

    🔷 update detail recipe

--- src/screen/DetailRecipe.jsx
+++ src/screen/DetailRecipe.jsx
@@ -9,7 +9,7 @@
 
 import React from 'react';
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
-import {Searchbar, useTheme} from 'react-native-paper';
+import {useTheme, DataTable, TextInput} from 'react-native-paper';
 import {
   ScrollView,
   StatusBar,
@@ -21,11 +21,13 @@ import {
   useWindowDimensions,
   ImageBackground,
   Image,
+  Button,
 } from 'react-native';
 import BottomNavbar from '../components/BottomNavbar';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 export default function DetailRecipe({navigation, route}) {
-  const [search, setSearch] = React.useState('');
+  const [comments, setComments] = React.useState(undefined);
   const theme = useTheme();
   const {height, width, scale, fontScale} = useWindowDimensions();
   const {image, title, first_name, last_name, ingredients} = route.params;
@@ -54,27 +56,19 @@ export default function DetailRecipe({navigation, route}) {
       textShadowRadius: 10,
     },
     Title: {
-      fontSize: 32,
-      // fontWeight: 900,
-      color: 'white',
-      textShadowColor: 'rgba(0, 0, 0, 0.75)',
-      textShadowOffset: {width: -1, height: 1},
-      textShadowRadius: 10,
+      fontFamily: 'Montserrat-Black',
+      fontSize: 24,
+      color: 'black',
     },
     Creator: {
+      fontFamily: 'Montserrat-Medium',
       fontSize: 14,
-      // fontWeight: 400,
-      color: 'white',
-      textShadowColor: 'rgba(0, 0, 0, 0.75)',
-      textShadowOffset: {width: -1, height: 1},
-      textShadowRadius: 2,
+      color: 'black',
     },
   });
 
   return (
-    <SafeAreaView
-      edges={['top', 'right', 'bottom', 'left']}
-      style={{height: height - 30}}>
+    <SafeAreaView edges={['top', 'right', 'bottom', 'left']}>
       <StatusBar backgroundColor="#c40900ff" />
       <View>
         <ImageBackground src={image} style={styles.headerImage}>
@@ -90,60 +84,119 @@ export default function DetailRecipe({navigation, route}) {
           }}>
           <View
             style={{
-              paddingHorizontal: theme.padding.containerHorizontal,
-              paddingVertical: theme.padding.containerHorizontal,
-              borderTopRightRadius: 20,
-              borderTopLeftRadius: 20,
+              paddingHorizontal: 24,
+              paddingVertical: 20,
+              borderTopRightRadius: 24,
+              borderTopLeftRadius: 24,
               backgroundColor: 'white',
               marginTop: 300,
             }}>
-            <View style={{marginTop: -100, marginBottom: 40}}>
+            <View>
               <Text style={styles.Title}>{title}</Text>
               <Text style={styles.Creator}>
                 {`Created by ${first_name} ${last_name}`}
               </Text>
             </View>
 
-            <View
-              style={{
-                flexDirection: 'row',
-                gap: 10,
-                marginBottom: 10,
-                padding: 4,
-              }}>
-              <Text style={{color: 'black', fontWeight: 900, fontSize: 24}}>
-                Ingredient
-              </Text>
+            <View style={{margin: 10}}>
               <Text
                 style={{
-                  color: theme.colors.gray40,
-                  fontWeight: 900,
-                  fontSize: 24,
+                  fontFamily: 'Lato-BlackItalic',
+                  fontSize: 20,
+                  color: 'black',
+                  marginVertical: 4,
                 }}>
-                Videos
+                Ingredients :
               </Text>
+              <DataTable>
+                {ingredients?.ingridient?.map((ingred, index) => {
+                  return (
+                    <DataTable.Row key={index}>
+                      <DataTable.Cell>
+                        <Text
+                          style={{
+                            fontFamily: 'Lato-Regular',
+                            color: 'black',
+                          }}>
+                          {ingred}
+                        </Text>
+                      </DataTable.Cell>
+                    </DataTable.Row>
+                  );
+                })}
+              </DataTable>
             </View>
 
-            <View style={{padding: 4}}>
-              {ingredients === undefined
-                ? null
-                : ingredients.ingridient.map((ingred, index) => {
-                    return <Text key={index}>{ingred}</Text>;
-                  })}
+            <View style={{margin: 10}}>
+              <Text
+                style={{
+                  fontFamily: 'Lato-BlackItalic',
+                  fontSize: 20,
+                  color: 'black',
+                  marginVertical: 4,
+                }}>
+                The Steps :
+              </Text>
+              <DataTable>
+                {ingredients?.steps?.map((ingred, index) => {
+                  return (
+                    <DataTable.Row key={index}>
+                      <DataTable.Cell style={{flex: 0}}>
+                        {index + 1 + '     '}
+                      </DataTable.Cell>
+                      <DataTable.Cell>
+                        <View style={{padding: 2}}>
+                          <Text
+                            style={{
+                              fontFamily: 'Lato-Regular',
+                              color: 'black',
+                              lineHeight: 22,
+                            }}>
+                            {ingred}
+                          </Text>
+                        </View>
+                      </DataTable.Cell>
+                    </DataTable.Row>
+                  );
+                })}
+              </DataTable>
             </View>
 
-            <View style={{padding: 4}}>
-              {ingredients === undefined
-                ? null
-                : ingredients.steps.map((ingred, index) => {
-                    return <Text key={index}>{ingred}</Text>;
-                  })}
+            <View style={{margin: 10, flexDirection: 'column', gap: 10}}>
+              <Text
+                style={{
+                  fontFamily: 'Montserrat-Bold',
+                  color: 'black',
+                }}>
+                Post Comment
+              </Text>
+
+              <TextInput
+                style={{paddingVertical: 15, fontFamily: 'Montserrat-Regular'}}
+                multiline={true}
+                numberOfLines={4}
+                mode="outlined"
+                outlineColor={theme.colors.gray20}
+              />
+
+              <Button title="send" color={theme.colors.tmRed} />
             </View>
 
+            <View style={{margin: 10}}>
+              <Text
+                style={{
+                  fontFamily: 'Montserrat-Bold',
+                  color: 'black',
+                }}>
+                Comments
+              </Text>
+
+              {comments === undefined ? <Text>No Comment Found</Text> : null}
+            </View>
           </View>
         </ScrollView>
       </View>
-      <BottomNavbar navigation={navigation} />
+      <BottomNavbar navigation={navigation} screenName="Explore" />
     </SafeAreaView>
   );
 }
