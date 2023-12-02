d1ae34650a0b40963a6cec268ffd9217d3172f06
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-12-03 00:43:24 +0700

    ✅ integrating with backend

--- src/screen/UserRegister.jsx
+++ src/screen/UserRegister.jsx
@@ -9,29 +9,30 @@
 
 import React from 'react';
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
-import {useTheme, TextInput, Button} from 'react-native-paper';
+import {
+  useTheme,
+  TextInput,
+  Button,
+  Snackbar,
+  Modal,
+  Portal,
+} from 'react-native-paper';
 import {
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
-  Dimensions,
-  useWindowDimensions,
-  ImageBackground,
-  Image,
-  TouchableNativeFeedback,
   TouchableWithoutFeedback,
+  ActivityIndicator,
 } from 'react-native';
 import BottomNavbar from '../components/BottomNavbar';
-import * as Icons from 'react-native-feather';
-import ProfileLink from '../components/ProfileLink';
 import ProfileHeader from '../components/ProfileHeader';
+import axios from 'axios';
+import {backendUrl} from '../config';
 
 export default function UserRegister({navigation, route}) {
   const theme = useTheme();
-  const {height, width, scale, fontScale} = useWindowDimensions();
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -39,15 +40,57 @@ export default function UserRegister({navigation, route}) {
   const [firstName, setFirstname] = React.useState('');
   const [lastName, setLastname] = React.useState('');
 
-  const [authError, setAuthError] = React.useState('');
-  const [inputError, setInputError] = React.useState(null);
-  const [dupEmail, setDupEmailError] = React.useState(null);
-
-  const [pageRegisterState, setPageRegisterState] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(false);
-  const [timeLeft, setTimeLeft] = React.useState(5);
+  const [snackMessage, setSnackMessage] = React.useState('');
+  const [visible, setVisible] = React.useState(false);
 
-  React.useEffect(() => {}, []);
+  React.useEffect(() => {}, [isLoading]);
+
+  const registerButtonHandler = () => {
+    setIsLoading(true);
+
+    if (password !== passwordc) {
+      setIsLoading(false);
+      setSnackMessage('Password Not Match');
+      setVisible(true);
+      return;
+    }
+
+    axios({
+      method: 'post',
+      url: `${backendUrl}/user/register`,
+      data: {
+        firstName,
+        lastName,
+        email,
+        password,
+      },
+    })
+      .then(() => {
+        setSnackMessage('Register Success');
+        setVisible(true);
+        navigation.navigate('UserLogin');
+      })
+      .catch(err => {
+        if (err.response.status === 422) {
+          setSnackMessage(String(err.response.data.message));
+          setVisible(true);
+        }
+
+        if (err.response.status === 409) {
+          setSnackMessage(String(err.response.data.massage));
+          setVisible(true);
+        }
+
+        if (err.response.status === 500) {
+          setSnackMessage(String(err.response.data.massage));
+          setVisible(true);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   const styles = StyleSheet.create({
     LinkBody: {
@@ -63,6 +106,46 @@ export default function UserRegister({navigation, route}) {
 
   return (
     <SafeAreaView style={{backgroundColor: 'white', flexGrow: 1}}>
+      <Snackbar
+        visible={visible}
+        onDismiss={() => setVisible(false)}
+        wrapperStyle={{top: 0}}
+        style={{
+          backgroundColor: 'white',
+          justifyContent: 'center',
+          zIndex: 999,
+        }}
+        action={{
+          label: 'Close',
+          labelStyle: {color: 'black'},
+          onPress: () => {
+            setSnackMessage('');
+          },
+        }}>
+        <Text style={{color: theme.colors.tmRed}}>{snackMessage}</Text>
+      </Snackbar>
+
+      <Portal>
+        <Modal
+          visible={isLoading}
+          onDismiss={() => {
+            //
+          }}
+          contentContainerStyle={{backgroundColor: 'transparent', padding: 20}}>
+          <View style={{justifyContent: 'center', gap: 10}}>
+            <ActivityIndicator size="large" color={theme.colors.tmRed} />
+            <Text
+              style={{
+                textAlign: 'center',
+                color: 'black',
+                fontFamily: 'Montserrat-Bold',
+              }}>
+              Loading
+            </Text>
+          </View>
+        </Modal>
+      </Portal>
+
       <StatusBar backgroundColor="#c40900ff" />
       <ScrollView
         showsVerticalScrollIndicator={false}
@@ -70,8 +153,6 @@ export default function UserRegister({navigation, route}) {
         {/* Header */}
         <ProfileHeader text1="Register" />
 
-        {/* Profile Links */}
-        {/* Not Login */}
         <View style={styles.LinkBody}>
           <Text
             style={{
@@ -165,7 +246,7 @@ export default function UserRegister({navigation, route}) {
             mode="contained"
             buttonColor={theme.colors.tmRed}
             style={{height: 50, borderRadius: 50, justifyContent: 'center'}}
-            onPress={() => console.log('Pressed')}>
+            onPress={registerButtonHandler}>
             <Text style={{fontFamily: 'Montserrat-Medium'}}>Register</Text>
           </Button>
 
@@ -186,11 +267,6 @@ export default function UserRegister({navigation, route}) {
             </TouchableWithoutFeedback>
           </View>
         </View>
-
-        {/* Login */}
-        {/* <View style={styles.profileLinkBody}>
-            <Text>Not Login</Text>
-          </View> */}
       </ScrollView>
       <BottomNavbar navigation={navigation} screenName="Profile" />
     </SafeAreaView>
